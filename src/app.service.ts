5a8401476773a58a8f388a9f0b2659d65482bbb0
wreeshab <evrishab@gmail.com>
2026-01-28 15:37:48 +0530

    feat: add helpful instructions on root route

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      message: 'Welcome to Mini CRM API',
+      documentation: 'API documentation is available at /api/docs',
+      endpoints: {
+        docs: '/api/docs',
+        auth: '/api/auth',
+        users: '/api/users',
+        customers: '/api/customers',
+        tasks: '/api/tasks',
+      },
+    };
   }
 }
